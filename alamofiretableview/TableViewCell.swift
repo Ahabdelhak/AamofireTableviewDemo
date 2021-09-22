e9d9980807960d05bcd360469338fa7b15d068d9
ah_abdelhak <ah_abdelhak@yahoo.com>
2021-09-22 17:02:50 +0200

    Initial commit

---
+++ alamofiretableview/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  alamofiretableview
+//
+//  Created by Ahmed Sabry on 22/09/2021.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
